948642b1768e18fd1afd55f57b731dabe79fb7f3
IMTku <165256927+IMTku@users.noreply.github.com>
2024-10-20 16:57:29 +0800

    Finish 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+//sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+const sumAll = function(start,end) {
+    if(start<0||end<0)return "ERROR";
+    if(!Number.isInteger(start)||!Number.isInteger(end)) return "ERROR";
+    if(start>end){
+        let buffer=start;
+        start=end;
+        end=buffer;
+    }
+    let sum=0;
+    for(let i=start;i<=end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

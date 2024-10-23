ab14ab98dbb7d9631a3fcef66aff79419df654ac
IMTku <165256927+IMTku@users.noreply.github.com>
2024-10-23 11:42:13 +0800

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+/* # Exercise 10 - Fibonacci
+
+Create a function that returns a specific member of the Fibonacci sequence:
+
+> A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+> In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+> To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+```javascript
+fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+fibonacci(6); // returns 8
+```*/
+
+const fibonacci = function(num) {
+    num=parseInt(num);
+    if(num<0)return "OOPS";
+    else if(num===0)return 0;
+    else if(num===1||num===2)return 1;
+    else return fibonacci(num-1)+fibonacci(num-2);
+           
 
 };
 
